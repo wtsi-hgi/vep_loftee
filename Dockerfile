581b67ddda92fcf3f2079f67af256c59b772de8f
gn5 <gn5@sanger.ac.uk>
2020-07-14 12:43:44 +0000

    add

---
+++ Dockerfile
@@ -0,0 +1,37 @@
+FROM ensemblorg/ensembl-vep:release_100.2
+
+WORKDIR /opt/vep/src
+
+USER root
+RUN apt update && apt install wget libcurl4-openssl-dev git -y
+RUN chmod -R a+rw /opt/vep/src
+
+USER vep
+WORKDIR /opt/vep/src
+RUN ls -ltra
+RUN wget https://github.com/samtools/samtools/releases/download/1.10/samtools-1.10.tar.bz2
+RUN tar -xvjf samtools-1.10.tar.bz2
+RUN mkdir /opt/vep/samtools
+WORKDIR /opt/vep/src/samtools-1.10
+RUN ./configure --without-curses --prefix=/opt/vep/samtools
+RUN make
+RUN make install
+ENV PATH /opt/vep/samtools/bin:$PATH
+RUN echo $PATH
+RUN ls -ltra /opt/vep/samtools
+RUN samtools --version
+
+
+WORKDIR /opt/vep/src
+RUN git clone https://github.com/DBD-SQLite/DBD-SQLite.git 
+WORKDIR /opt/vep/src/DBD-SQLite
+RUN mkdir /opt/vep/DBD-SQLite
+RUN perl Makefile.PL PREFIX=/opt/vep/DBD-SQLite
+RUN make
+RUN make test
+RUN make install
+ENV PERL5LIB /opt/vep/DBD-SQLite:$PERL5LIB
+RUN echo $PERL5LIB
+RUN ls -ltra /opt/vep/src/ensembl-vep
+RUN echo $PATH
+WORKDIR /opt/vep/src/ensembl-vep
---
+++ make_vep_cache.sh
@@ -0,0 +1,37 @@
+vep_release=100.2 # this must match the version of the docker image
+genome=GRCh38
+vep_docker_image=4ee408888558
+local_vep_cache=${HOME}/vep_data_${vep_release}.${genome}
+echo ${local_vep_cache}
+
+rm -rf ${local_vep_cache}
+mkdir -p ${local_vep_cache}
+chmod a+rwx ${local_vep_cache}
+
+# instructions from
+# http://www.ensembl.org/info/docs/tools/vep/script/vep_download.html#docker
+echo install cache
+sudo docker run -t -i \
+     -v ${local_vep_cache}:/opt/vep/.vep \
+      ${vep_docker_image} \
+      perl INSTALL.pl -a cfp -s homo_sapiens -y ${genome} --PLUGINS all
+
+# add loftee plugin to the cache
+
+# add loftee plugin
+# instructions from https://github.com/konradjk/loftee
+echo add loftee plugin to cache
+git clone \
+    https://github.com/konradjk/loftee.git \
+    tmp_clone && \
+    rm -rf tmp_clone/.git* && \
+    cp -r tmp_clone/* ${local_vep_cache}/Plugins/ && \
+    rm -r tmp_clone
+
+# download optional loftee files human_ancestor_fa
+wget https://s3.amazonaws.com/bcbio_nextgen/human_ancestor.fa.gz -P ${local_vep_cache}/
+wget https://s3.amazonaws.com/bcbio_nextgen/human_ancestor.fa.gz.fai -P ${local_vep_cache}/
+wget https://s3.amazonaws.com/bcbio_nextgen/human_ancestor.fa.gz.gzi -P ${local_vep_cache}/
+# download optional loftee files conservation_file
+wget https://personal.broadinstitute.org/konradk/loftee_data/GRCh37/phylocsf_gerp.sql.gz -P ${local_vep_cache}/
+gunzip ${local_vep_cache}/phylocsf_gerp.sql.gz
