cbb904002bea5e396e8902f3193b7f1934a3702f
Guillaume Noell <50482094+gn5@users.noreply.github.com>
2020-10-19 15:49:53 +0100

    Update make_vep_cache.sh

--- make_vep_cache.sh
+++ make_vep_cache.sh
@@ -34,6 +34,9 @@ if [ $genome = "GRCh37" ]; then
    wget https://s3.amazonaws.com/bcbio_nextgen/human_ancestor.fa.gz -P ${local_vep_cache}/
    wget https://s3.amazonaws.com/bcbio_nextgen/human_ancestor.fa.gz.fai -P ${local_vep_cache}/
    wget https://s3.amazonaws.com/bcbio_nextgen/human_ancestor.fa.gz.gzi -P ${local_vep_cache}/
+   wget https://personal.broadinstitute.org/konradk/loftee_data/GRCh37/GERP_scores.exons.txt.gz -P ${local_vep_cache}/                                     
+   wget https://personal.broadinstitute.org/konradk/loftee_data/GRCh37/GERP_scores.final.sorted.txt.gz -P ${local_vep_cache}/                              
+   wget https://personal.broadinstitute.org/konradk/loftee_data/GRCh37/GERP_scores.final.sorted.txt.gz.tbi -P ${local_vep_cache}/                   
    # download optional loftee files conservation_file
    wget https://personal.broadinstitute.org/konradk/loftee_data/GRCh37/phylocsf_gerp.sql.gz -P ${local_vep_cache}/
    gunzip ${local_vep_cache}/phylocsf_gerp.sql.gz
